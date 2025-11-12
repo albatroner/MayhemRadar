cf375281f47cd5646eee472333a0eeba363e5941
Alex <tokenpoly.org@gmail.com>
2025-11-12 22:49:37 +0300

    add vercel edge functions

---
+++ api/ca.js
@@ -0,0 +1,13 @@
+import { get } from '@vercel/edge-config'
+
+export default async function getCA(req, res) {
+    try {
+        const ca = await get('ca');
+        if (!ca) {
+            return res.status(404).json({ success: false, error: 'CA not found' });
+        }
+        res.status(200).json({ success: true, ca: ca });
+    } catch (error) {
+        res.status(500).json({ success: false, error: 'Failed to get CA' });
+    }
+}
